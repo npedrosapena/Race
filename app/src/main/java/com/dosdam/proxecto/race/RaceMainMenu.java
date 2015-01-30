9067525f705e5d145db6d7232d5f45d343f5a0d1
nelson <npedrosapena@danielcastelao.org>
2015-01-30 21:11:00 +0100

    agregados botones start y exit

--- app/src/main/java/com/dosdam/proxecto/race/RaceMainMenu.java
+++ app/src/main/java/com/dosdam/proxecto/race/RaceMainMenu.java
@@ -2,6 +2,8 @@ package com.dosdam.proxecto.race;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 
 /**
  * Created by nelson on 30/01/15.
@@ -14,6 +16,51 @@ public class RaceMainMenu extends Activity
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main); //en el main cargamos el layout con la imagen del coche (splashscreen)
+
+        final RaceEngine engine = new RaceEngine();
+
+         /*vamos a instancias los botones para poder darles propiedades*/
+        ImageButton start= (ImageButton)findViewById(R.id.btnStart);
+        ImageButton exit=(ImageButton)findViewById(R.id.btnExit);
+
+        start.getBackground().setAlpha(RaceEngine.MENU_BUTTON_ALPHA); //añadimos canal alpha con el valor 0
+        start.setHapticFeedbackEnabled(RaceEngine.HAPTIC_BUTTON_FEEDBACK); //activamos propiedades especiales
+
+        exit.getBackground().setAlpha(RaceEngine.MENU_BUTTON_ALPHA);
+        exit.setHapticFeedbackEnabled(RaceEngine.HAPTIC_BUTTON_FEEDBACK);
+
+
+        /* le añadimos el click a los botones */
+
+        start.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                /* Empieza el juego*/
+            }
+        });
+
+
+        exit.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                boolean clean=false;
+                clean=engine.onExit(v);
+
+                if(clean)
+                {
+                    int pid=android.os.Process.myPid(); //recogemos la clave interna de la app
+                    android.os.Process.killProcess(pid); //cerramos la aplicacion limpiamente y todos sus hilos
+                }
+            }
+        });
+
+
+
+
 
     }
 }
