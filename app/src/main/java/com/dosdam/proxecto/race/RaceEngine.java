b28cf8e7c6e2f1675348698cdfa5700853f9ab87
nelson <npedrosapena@danielcastelao.org>
2015-01-30 21:11:22 +0100

    agregado metodo salir

--- app/src/main/java/com/dosdam/proxecto/race/RaceEngine.java
+++ app/src/main/java/com/dosdam/proxecto/race/RaceEngine.java
@@ -1,9 +1,30 @@
 package com.dosdam.proxecto.race;
 
+import android.util.Log;
+import android.view.View;
+import android.widget.Toast;
+
 /**
  * Created by nelson on 30/01/15.
  */
 public class RaceEngine
 {
     public static final int GAME_THREAD_DELAY=4000;
+    public static final int MENU_BUTTON_ALPHA=0; //se usará mas tarde para quitar el fondo a las imagenes de los botones
+    public static final boolean HAPTIC_BUTTON_FEEDBACK=true; //el feedback es una opcion que nos habilita muchos pequeños detalles tal como
+                                                             //vibrar al pulsar una tecla,  volver atrás en los dispositivos con boton táctil...
+
+    /* metodo para salir del juego */
+
+    public boolean onExit(View v)
+    {
+        try
+        {
+         return true;
+        }catch(Exception ex)
+        {
+            Log.e("error saliendo del juego",ex.getMessage().toString());//si se produce un error que nos informe por consola
+            return false;
+        }
+    }
 }
