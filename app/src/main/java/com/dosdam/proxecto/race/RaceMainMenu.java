27930b4963d7a21d74d70edf1364bc4084b53146
nelson <npedrosapena@danielcastelao.org>
2015-01-30 19:24:20 +0100

    el splash lleva a esta clase en la que tenemos el menu

---
+++ app/src/main/java/com/dosdam/proxecto/race/RaceMainMenu.java
@@ -0,0 +1,19 @@
+package com.dosdam.proxecto.race;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by nelson on 30/01/15.
+ */
+
+public class RaceMainMenu extends Activity
+{
+
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main); //en el main cargamos el layout con la imagen del coche (splashscreen)
+
+    }
+}
