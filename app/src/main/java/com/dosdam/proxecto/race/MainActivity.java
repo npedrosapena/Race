659dc2e7dfcac685dd06445e07ff46d16b906097
nelson <npedrosapena@danielcastelao.org>
2015-01-30 19:23:18 +0100

    control de ocultar la barra. asocio el layout de la clase main activity al layout splash, que es la que tiene la foto. Creo un thread para poder hacer un fade in y fade out controlado por la variable GAME_THREAD_DELAY

--- app/src/main/java/com/dosdam/proxecto/race/MainActivity.java
+++ app/src/main/java/com/dosdam/proxecto/race/MainActivity.java
@@ -1,9 +1,13 @@
 package com.dosdam.proxecto.race;
 
+import android.content.Intent;
+import android.os.Build;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.os.Handler;
+import android.view.WindowManager;
 
 
 public class MainActivity extends ActionBarActivity
@@ -12,8 +16,35 @@ public class MainActivity extends ActionBarActivity
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
+        // If the Android version is lower than Jellybean, use this call to hide
+        // the status bar.
+        if (Build.VERSION.SDK_INT < 16) {
+            getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                    WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        }
+
+
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.splashscreen); //en el main cargamos el layout con la imagen del coche (splashscreen)
+
+
+        /*Creamos un nuevo hilo para el juego y sus recursos*/
+
+
+        new Handler().postDelayed(new Thread()
+        {
+            @Override
+            public void run()
+            {
+                Intent mainMenu = new Intent(MainActivity.this, RaceMainMenu.class);
+                MainActivity.this.startActivity(mainMenu);
+                MainActivity.this.finish();
+                overridePendingTransition(R.layout.fadein,R.layout.fadeout);
+            }
+        }, RaceEngine.GAME_THREAD_DELAY);
+
+
+
     }
 
 
