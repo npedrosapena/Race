a6c044f81fd1cb6dd3e31145cc659e0c0fe195c2
nelson <npedrosapena@danielcastelao.org>
2015-01-31 11:19:49 +0100

    creado objeto tipo hilo

--- app/src/main/java/com/dosdam/proxecto/race/RaceEngine.java
+++ app/src/main/java/com/dosdam/proxecto/race/RaceEngine.java
@@ -1,5 +1,6 @@
 package com.dosdam.proxecto.race;
 
+import android.content.Context;
 import android.util.Log;
 import android.view.View;
 import android.widget.Toast;
@@ -14,6 +15,12 @@ public class RaceEngine
     public static final boolean HAPTIC_BUTTON_FEEDBACK=true; //el feedback es una opcion que nos habilita muchos pequeños detalles tal como
                                                              //vibrar al pulsar una tecla,  volver atrás en los dispositivos con boton táctil...
 
+    public static final int SPLASH_SCREEN_MUSIC=R.raw.audiointro; //indicamos cual es el archivo a reproducir en el menu
+    public static final int R_VOLUME=100; //nivel audio derecha
+    public static final int L_VOLUME=100; //nivel audio izda
+    public static final boolean LOOP_BACKGROUND_MUSIC=true; //reproducimos en loop
+    public static Context context; //"guarda" el hilo en el que se reproduce el audio, así podemos hacer cosas con ese hilo (context)
+    public static Thread musicThread; //hilo para reproducir musica
     /* metodo para salir del juego */
 
     public boolean onExit(View v)
